4c11b00a17388bb022b22bf87100610798b2df9b
Brandon <github@briley.me>
2023-08-31 22:04:14 -0600

    Movement and Camera
    Updated the player controller script and added a script for the camera movement.

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float gravity = -20f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private int availableJumps; // Track the remaining jumps
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        availableJumps = 2; // Initialize available jumps to 2 (double jump)
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+
+        // Horizontal movement
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector3(0, 0, moveInput);
+        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
+
+        // Apply gravity
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+
+            // Reset available jumps when grounded
+            availableJumps = 2;
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded || availableJumps > 0)
+            {
+                if (!isGrounded)
+                {
+                    availableJumps--; // Decrement available jumps if double jumping
+                }
+
+                velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+            }
+        }
+
+        // Apply movement and gravity
+        controller.Move((move + velocity) * Time.deltaTime);
     }
 }
